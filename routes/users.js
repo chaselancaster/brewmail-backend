8abb2cf528d46874bf177aa6c3ccd211425c9212
chaselancaster <chaseblancaster@gmail.com>
2019-06-04 15:28:55 -0700

    Add user register route

--- routes/users.js
+++ routes/users.js
@@ -3,3 +3,20 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 
 const User = require("../models/User");
+
+// Register
+router.post("/register", async (req, res) => {
+  console.log("register post route hit");
+  try {
+    const createdUser = await User.create(req.body);
+    if (createdUser) {
+      req.session.dbId = createdUser._id;
+      res.json({
+        user: createdUser,
+        success: true
+      });
+    }
+  } catch (err) {
+    res.json({ err });
+  }
+});
