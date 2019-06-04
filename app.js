838cbcf9a5d1e5fc3a3252fe100a3bde57621414
chaselancaster <chaseblancaster@gmail.com>
2019-06-04 10:37:45 -0700

    add app.use for userRoutes and export app

--- app.js
+++ app.js
@@ -11,3 +11,27 @@ require("./db/db");
 
 // const apiRouter = require("./routes/api");
 const userRouter = require("./routes/users");
+
+const app = express();
+
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "hello",
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+app.use("/users", userRouter);
+
+// Catching error
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+module.exports = app;
