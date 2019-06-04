c2f53a8add69d5d31cf42f6623acab1fa0b68f05
chaselancaster <chaseblancaster@gmail.com>
2019-06-04 10:32:09 -0700

    require db, userRouter, and app

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
 const session = require("express-session");
+
+require("dotenv").config();
+
+require("./db/db");
+
+// const apiRouter = require("./routes/api");
+const userRouter = require("./routes/users");
