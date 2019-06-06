62991cc8ff7d1b8950dc79ef93fbb2443d7d002c
chaselancaster <chaseblancaster@gmail.com>
2019-06-06 16:42:25 -0700

    add post method to add beer to user ISO list

--- routes/beers.js
+++ routes/beers.js
@@ -42,4 +42,23 @@ router.post("/add/fortrade", async (req, res) => {
   }
 });
 
+// Add beer to user ISO
+router.post("/add/iso", async (req, res) => {
+  try {
+    console.log(req.session.dbId, "req.session.dbId");
+    const currentUser = await User.findById(req.body.currentUser._id);
+    console.log(currentUser, "currentUser");
+    delete req.body.currentUser;
+    currentUser.isoBeer.push(req.body);
+    currentUser.save();
+    res.json({
+      user: currentUser,
+      success: true,
+      message: "Added to for trade"
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 module.exports = router;
