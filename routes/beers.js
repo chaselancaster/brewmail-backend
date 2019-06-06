d42e913ba8f43e8d20c01199d11bb396e0469a23
chaselancaster <chaseblancaster@gmail.com>
2019-06-06 12:03:56 -0700

    change post route in beers.js so that the found user acts as currentUser

--- routes/beers.js
+++ routes/beers.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const Beer = require("../models/Beer");
+const User = require("../models/User");
 
 // Add beer to User cellar
 router.post("/add", async (req, res) => {
   try {
     const currentUser = await User.findById(req.session.dbId);
-    foundUser.cellarBeer.push(req.body);
-    foundUser.save();
+    currentUser.cellarBeer.push(req.body);
+    currentUser.save();
     res.json({
       user: currentUser,
       success: true,
