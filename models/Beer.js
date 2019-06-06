631f6b5124dc9745dd7b3896332cb71cd7dc6ddc
chaselancaster <chaseblancaster@gmail.com>
2019-06-05 21:06:06 -0700

    add BeerSchema

---
+++ models/Beer.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const BeerSchema = new mongoose.Schema({
+  beerName: { type: String },
+  beerABV: { type: String, required: true },
+  beerStyle: { type: String, required: true },
+  breweryName: { type: String },
+  quantity: { type: Number },
+  year: { type: String },
+  size: { type: String },
+  label: { type: String }
+});
+
+export default BeerSchema;
