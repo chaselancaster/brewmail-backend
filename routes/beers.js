0e4e10b6c8c2d20d5045910f0506da67e269fc6a
chaselancaster <chaseblancaster@gmail.com>
2019-06-05 21:20:37 -0700

    Add /add post route in beers.js

--- routes/beers.js
+++ routes/beers.js
@@ -2,3 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 const Beer = require("../models/Beer");
+
+// Add beer to User cellar
+router.post("/add", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.dbId);
+    foundUser.cellarBeer.push(req.body);
+    foundUser.save();
+    res.json({
+      user: currentUser,
+      success: true,
+      message: "Added to cellar"
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
