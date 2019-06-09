27a3a84f48e3ccffb83a36b9a968c4eb5779e22a
chaselancaster <chaseblancaster@gmail.com>
2019-06-09 15:27:50 -0700

    require user in trades.js

--- routes/trades.js
+++ routes/trades.js
@@ -1,20 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/createtrade", async (req, res) => {
+const User = require("../models/User");
+
+router.post("/create/:currentUserId/:tradingPartnerId", async (req, res) => {
   try {
     console.log(req.session.dbId, "req.session.dbId");
-    const currentUser = await User.findById(req.body.currentUser._id);
-    console.log(currentUser, "currentUser");
-    delete req.body.currentUser;
-    currentUser.trades.push(req.body);
-    currentUser.save();
-    res.json({
-      currentUser: currentUser,
-      success: true,
-      message: "Trade has been created"
-    });
+    const tradeCreator = await User.findById(req.params.currentUserId);
+    const tradePartner = await User.findById(req.params.tradingPartnerId);
+    console.log(tradeCreator, "<-- tradeCreator in create trade function");
+    console.log(tradePartner, "<-- tradePartner in create trade function");
+    // currentUser.trades.push(req.body);
+    // currentUser.save();
+    // res.json({
+    //   currentUser: currentUser,
+    //   success: true,
+    //   message: "Trade has been created"
+    // });
   } catch (err) {
     console.log(err);
   }
 });
+
+module.exports = router;
