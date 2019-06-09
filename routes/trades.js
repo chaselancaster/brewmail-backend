a64aa55aa1ff16302d0dcdab72ea3f60475752f2
chaselancaster <chaseblancaster@gmail.com>
2019-06-09 16:01:33 -0700

    install uuid

--- routes/trades.js
+++ routes/trades.js
@@ -5,18 +5,22 @@ const User = require("../models/User");
 
 router.post("/create/:currentUserId/:tradingPartnerId", async (req, res) => {
   try {
-    console.log(req.session.dbId, "req.session.dbId");
     const tradeCreator = await User.findById(req.params.currentUserId);
     const tradePartner = await User.findById(req.params.tradingPartnerId);
     console.log(tradeCreator, "<-- tradeCreator in create trade function");
     console.log(tradePartner, "<-- tradePartner in create trade function");
-    // currentUser.trades.push(req.body);
-    // currentUser.save();
-    // res.json({
-    //   currentUser: currentUser,
-    //   success: true,
-    //   message: "Trade has been created"
-    // });
+    // push trade into tradeCreator's and tradePartner's trades array
+    tradeCreator.trades.push(req.body);
+    tradeCreator.save();
+    tradePartner.trades.push(req.body);
+    tradePartner.save();
+    console.log(tradeCreator, "<-- tradeCreator after trade has been added");
+    console.log(tradePartner, "<-- tradePartner after trade has been added");
+    res.json({
+      currentUser: tradeCreator,
+      success: true,
+      message: "Trade has been created"
+    });
   } catch (err) {
     console.log(err);
   }
